868d537d0cfc6697db47ec6e2ef059c22deb03aa
iuniperum <julpietrzak147@gmail.com>
2025-04-18 23:31:13 +0200

    jest dobrze

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -4,6 +4,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace lab7;
 
@@ -12,7 +13,8 @@ public partial class MainWindow : Window
     private string input = "";
     
     private string czworki = "";
-    private string[] lista;
+    //private string[] lista;
+    //private string[] unikalne;
     private string sprawdzana = "";
     private string test = "";
     
@@ -24,27 +26,29 @@ public partial class MainWindow : Window
     }
     
     private void zlicz(object sender, RoutedEventArgs e) {
+        List<string> lista = new List<string>();
+        List<string> unikalne = new List<string>();
         input = Convert.ToString(sekwencja.Text);
         for (int i = 0; i < (input.Length - 3); i++) {
             czworki = czworki + input.Substring(i, 4) + " ";
         }
-        lista = czworki.Split(" ");
+        lista = czworki.Split(" ").ToList();
+        unikalne = lista.Distinct().ToList();
         
-        for (int i = 0; i < (lista.Length); i++) {
-            if (lista[i] != "xxxx") {
-                sprawdzana = lista[i];
-                for (int j = 0; j < (lista.Length); j++) {
-                    if (lista[j] == sprawdzana) {
-                        licznik += 1;
-                        lista[j] = "xxxx";
-                    }
-                    output = output + sprawdzana + ": " + licznik + "\n";
-                    licznik = 0;
+        for (int i = 0; i < (unikalne.Count - 1); i++) {
+            output += unikalne[i];
+            output += ": ";
+            for (int j = 0; j < lista.Count; j++) {
+                if (lista[j] == unikalne[i]) {
+                    licznik += 1;
                 }
             }
+            output += licznik.ToString();
+            output += "\n";
+            licznik = 0;
         }
-        for (int j = 0; j < (lista.Length); j++) {
-            test +=  lista[j] + "/";
+        for (int j = 0; j < (unikalne.Count); j++) {
+            test +=  unikalne[j] + "/";
         }
         wynik.InnerLeftContent = output;
     }
--- obj/Debug/net9.0/lab7.AssemblyInfo.cs
+++ obj/Debug/net9.0/lab7.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+524e256ea9ab7f0d376d4b56cb29e059cb8abe28")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
